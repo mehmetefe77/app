90c8f0f1cf9e0c63825514d4c413c5f26e801c14
Glitch (hello-webpage) <none>
2020-02-10 15:39:11 +0000

    ⏰⛴ Checkpoint ./package.json:11255422/655 ./berke.js:11254937/118

---
+++ berke.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+    response.sendStatus(200);
+    });
+    app.listen(process.env.PORT);
+    setInterval(() => {
+    http.get('http://boost-aktif.glitch.me/');
+    }, 3000);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+const berke = new Discord.Client();
+const berke1 = new Discord.Client();
+const berke2 = new Discord.Client();
+const berke3 = new Discord.Client();
+const berke4 = new Discord.Client();
+const berke5 = new Discord.Client();
+const berke6 = new Discord.Client();
+const berke7 = new Discord.Client();
+const berke8 = new Discord.Client();
+const berke9 = new Discord.Client();
+const berke10 = new Discord.Client();
+const berke11 = new Discord.Client();
+const berke12 = new Discord.Client();
+const berke13 = new Discord.Client();
+const berke14 = new Discord.Client();
+const berke15 = new Discord.Client();
+const berke16 = new Discord.Client();
+const berke17 = new Discord.Client();
+const berke18 = new Discord.Client();
+const berke19 = new Discord.Client();
+const berke20 = new Discord.Client();
+const berke21 = new Discord.Client();
+const berke22 = new Discord.Client();
+const berke23 = new Discord.Client();
+const berke24 = new Discord.Client();
+const berke25 = new Discord.Client();
+const berke26 = new Discord.Client();
+const berke27 = new Discord.Client();
+const berke28 = new Discord.Client();
+const berke29 = new Discord.Client();
+const berke30 = new Discord.Client();
+const berke31 = new Discord.Client();
+const berke32 = new Discord.Client();
+const berke33 = new Discord.Client();
+const berke34 = new Discord.Client();
+const berke35 = new Discord.Client();
+const berke36 = new Discord.Client();
+
+berke1.login("Njc2MTM4NDEwMTU4NzE4OTc2.XkBVhw.LB6dw7e9vSKreR8DIKQdtHdtMoU");
+berke2.login("Njc2MTQ2MTY2NTA0ODgyMTg2.XkBdOg.cRdux1zpCKoMWkSyDYm6o6eE_ZQ");
+berke3.login("Njc2MTQ3OTY2NTk1MzAxNDI3.XkBe8Q.yocDBP68ckhuJKvGn_WYlapeGeU");
+berke4.login("Njc2MDUwNDE5MTM0MzY1NzA3.XkADmQ.fMudS2BXrsxZ0bSdkhR23AEYFk4");
+berke5.login("NjAzODUyODM4Mjc1MTIxMTY1.XkBf7Q.OJdhmazgUXTQ5_xhKBtAWzSWE7I");
+berke6.login("Njc2NDQxNTI5Nzk0OTUzMjE2.XkFvZg.SuyHRiNGPUhAVqpMuLSjL1Vry90");
+berke7.login("jc2NDQyMDUxMjUyMDYwMjIx.XkFwng.UN-QuJDGdMkQLGYO_Pfxv6fFnOM");
+berke8.login("Njc2NDQzNjk3MjMxMDM2NDM3.XkF3SQ.1kqt79Pz5jZLjUZMzV9K0BtrxC0");
+berke9.login("NjYwNTc4NTg2NDQ4NjI1Njc1.Xge6PA.lq9T0SLv-NU_5Aum2HVQ6mxg3BA");
+berke10.login("NjYwNTgyNzc0NzM3OTkzNzQ5.Xge96g.HfoR2wB8BR6Z-HBXYG48YNPMAro");
+berke11.login("NjYwNTg0NjUyOTYwMTcwMDM0.Xge_tA.Zy9BIU_N9vKnDwFnkrNvMzVvh-g");
+berke12.login("NjYwNTg0NjUyOTYwMTcwMDM0.Xge_tA.Zy9BIU_N9vKnDwFnkrNvMzVvh-g");
+berke13.login("NjYwNTg2ODc0NDYwNzAwNjg3.XgfCyA.AP6imqWmQtSQLOLBZ_0H7wVSpAc");
+berke14.login("NjYwNTg4Mzg1MzIwMTczNTk4.XgfDdw.eC6upUtEaSLTqGX8JxuVLSYQbQw");                      
+berke15.login("NjYwNTg5MzQ5NDEwMzA4MTM2.XgfEEw.-bRsDocSPY1WPZCLU7HAPbwSCZU");
+berke16.login("NjYwNTg5OTQ1MzA3NjYwMzA5.XgfFFw.Qgf0BPvhgutZ7KXG2tA8_ILqsos");
+berke17.login("NjYwNTkwOTQ4NTA1NDg1MzMy.XgfF_A.0MlGBCGirb6TSmafi0lNV7uA2z4");
+berke18.login("NjYwNTkyNjg0NjI4NTc0MjU5.XgfHYg.dLAaitf0uZ7kzrkCPiQ9qt5X54Y");
+berke19.login("NjYwNTkzNDE4Mzk2ODkzMjA3.XgfIRQ.kLnh6e9awmm_kywz2G_MtpgEQ2c");
+berke20.login("NTg5ODg4NDc3MDY4MjYzNDM0.XQaO5A.BlGamvF_PzDNKzZEknG1EIU6Tc4");
+berke21.login("NDwYWG3L0olJ1NTNhIqyuBqc.DoCsF6.IP7rFR0HhQEZZxTPb15ieTEwoko");
+berke22.login("NDtX7Xg1Q4PVkTPvzNf8rNUW.DOH8LO.gVTGnHJL08VFSdbcwu4oWx3cwxB");
+berke23.login("NDJdv0EnPKO9fJ7fEPLZt0jq.DDi2Ve.Q64dyomett132Uup49ayXdYRnsO");
+berke24.login("NDQOd46wWUrOv7J9sxhg1pNt.Dw8fn3.54LVjbI3yk59p4BcIHWxqpoMJLY");
+berke25.login("NTg5ODk0ODI2MDg3MDIyNzI3.XQaU7Q.hkv_bhqv_QxDRp3_eIGWiPyftmo");
+berke26.login("NDG7x58j4o5fPF4jachGxBS4.DAnzGs.KRj5Uirp9S3RCypLNCDlSUU2gaT");
+berke27.login("NjY0NTAzMjk1MTI5Mjg4NzI0.XhYBIA.l2Tqo-NOMRwqAIJ3x6rseewslXQ");
+berke28.login("NjYwNTk0NTM1NTQ5ODk0Njk4.XgfI4w.UbG_hxKop25VaI9uok8XdoaKwSk");
+berke29.login("NjYwNTcyNjQ0NDYyMjk3MDg5.Xge0qw.VU5UGYio_rihe8hpBOkGHZZ2uW0");
+berke30.login("NjY4ODA4MDgxNjM0NzU0NTkz.XiWrkw.mq8CdWy5MBUJ_eXwUnCjTFTIT5E");
+berke31.login("NjQ4NTc0Mzk2MDMzNzk0MDY4.XdwORg.FOPz2F2zyxd2o6rVKD6_O_Oam6c");
+berke32.login("NjQ4NTc0Mzk2MDMzNzk0MDY4.XdwORg.FOPz2F2zyxd2o6rVKD6_O_Oam6c");
+berke33.login("NTg5ODY0NzM0MDkwNTkyMjU3.XQZ4fg.F_gvFxfVVSkk3zuAjagcBjYWUIs");
+berke34.login("NTg5ODkwMTgzNjkyOTQzMzc5.XQaQrg.xU_ysoKDL1a9C5O2FtJouOKaGZc");
+berke35.login("NjY0NTA3MjM4MTE4OTgxNjQy.XhYEyw.JPVAn2AtWdslGPDtuP61u76p_QM");
+berke36.login("NTg5ODYxNDYxNjY5NTc2NzA2.XQZ1mQ.ixocSTt5MVn4xjr62fS8VCZnEPg");
+
+
+
+
+
+
+
+
+//✭ https://discord.gg/HwXFY7a//
